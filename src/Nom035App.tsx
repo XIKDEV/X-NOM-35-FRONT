7d00bf81b34e9590c95dba794c66f2e8c38d2a45
Axel Coronado <42565749+ax3l98@users.noreply.github.com>
2023-08-10 16:25:06 -0700

    feat(Project): :sparkles: Creation work space to project
    It is init project and new dependencies

---
+++ src/Nom035App.tsx
@@ -0,0 +1,9 @@
+function Nom035App() {
+  return (
+    <>
+      <h1>X Nom-035</h1>
+    </>
+  );
+}
+
+export default Nom035App;
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Nom035app from './Nom035App.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Nom035app />
+  </React.StrictMode>
+);
