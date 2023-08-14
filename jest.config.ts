20e45d11f27cf6d3c7a45f6cc31c59e2adaefef0
Alderick Hernán Rios Contreras <93097571+XIKDEV@users.noreply.github.com>
2023-08-14 09:52:48 -0700

    fix(test): :white_check_mark: Add coverage to testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,14 @@
-/** @type {import('jest').Config} */
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFiles: ['./jest.setup.ts'],
-  transformIgnorePatterns: [],
-  moduleNameMapper: {
-    '\\.(css|less)$': 'identity-obj-proxy',
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
+  coverageReporters: ['lcov', 'text-summary'],
 };
--- src/test/auth/pages/LoginPage.test.tsx
+++ src/test/auth/pages/LoginPage.test.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-
 import { render } from '@testing-library/react';
-import { LoginPage } from '../../../src/auth/pages/LoginPage';
+import { LoginPage } from '../../../auth/pages';
 
 describe('Test in <LoginPage />', () => {
   test('should be match with the snapshot', () => {
